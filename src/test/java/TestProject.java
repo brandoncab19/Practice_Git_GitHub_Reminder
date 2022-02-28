7924e5b1be0c029bed19d0f571944f5bc8f1ba00
brandoncabrera <brandonfcab@gmail.com>
2022-02-28 07:46:52 -0500

    committing through the test folder

--- src/test/java/TestProject.java
+++ src/test/java/TestProject.java
@@ -1,5 +1,6 @@
 public class TestProject {
     public static void main(String[] args) {
         System.out.println("Testing testing");
+        System.out.println("adding code through the test folder");
     }
 }
